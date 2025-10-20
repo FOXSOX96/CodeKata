9fa3fe96a4e8157dddf57297b18ecb867f6d3df2
WonTaeJun <dnjsxonws94@gmail.com>
2025-10-20 14:32:50 +0900

    feat : CodeKata002

---
+++ src/CodeKata002.java
@@ -0,0 +1,17 @@
+public class CodeKata002 {
+
+
+    public int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        CodeKata002 code = new CodeKata002();
+        System.out.println(code.solution(3, 4));
+        System.out.println(code.solution(27, 19));
+    }
+}
+
+
